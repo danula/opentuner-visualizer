446ea1ffca28d7e5e5870cfbc13d6be8e5d1716c
Isuru Fernando <isuruf@gmail.com>
2015-08-14 15:06:08 +0530

    update constants

--- constants.py
+++ constants.py
@@ -1,1 +1,4 @@
-database_url = "/home/kasun/projects/Dacapo_Tuned_DataCollection/10.8.106.242/ubuntu4.db"
+import os
+#database_url = "/home/isuru/Desktop/Dacapo_Tuned_DataCollection/10.8.106.242/ubuntu4.db"
+database_url = "/home/isuru/Desktop/opentuner.db/madawa-notebook.db"
+manipulator_url = os.path.join(os.path.join(os.path.dirname(database_url), os.pardir), "params")
